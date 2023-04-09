e34f29ae898867dc5f6bd2a0a98d7e17ef73534d
Emre GÜZEL <eguzel9@ford.com.tr>
2023-04-09 05:33:47 +0300

    Employee CRUD Operations

---
+++ MVCCRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVCCRUD.Data;
+using MVCCRUD.Models;
+using MVCCRUD.Models.Domain;
+
+namespace MVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext) 
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees = await mvcDemoDbContext.Employees.ToListAsync(); 
+            return View(employees);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequset)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequset.Name,
+                Email = addEmployeeRequset.Email,
+                Salary = addEmployeeRequset.Salary,
+                Department = addEmployeeRequset.Department,
+                DateOfBirth = addEmployeeRequset.DateOfBirth
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> View(Guid id) 
+        {
+            var selectedEmployee = await mvcDemoDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(selectedEmployee != null)
+            {
+                var viewModel = new Employee()
+                {
+                    Id =selectedEmployee.Id,
+                    Name = selectedEmployee.Name,
+                    Email = selectedEmployee.Email,
+                    Salary = selectedEmployee.Salary,   
+                    Department = selectedEmployee.Department,
+                    DateOfBirth = selectedEmployee.DateOfBirth
+                };
+                return await Task.Run(() => View("View" , viewModel));
+            }
+
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> View(Employee model)
+        {
+            var employee = await mvcDemoDbContext.Employees.FindAsync(model.Id);
+            if(employee != null)
+            {
+                employee.Name = model.Name; 
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.Department = model.Department;
+                employee.DateOfBirth = model.DateOfBirth;
+
+                await mvcDemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Employee model)
+        {
+            var employee =await mvcDemoDbContext.Employees.FindAsync(model.Id);
+            if (employee != null)
+            {
+                mvcDemoDbContext.Employees.Remove(employee);
+                await mvcDemoDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MVCCRUD/Program.cs
+++ MVCCRUD/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using MVCCRUD.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MVCDemoDbContext>(options =>
+options.UseSqlServer(builder.Configuration
+.GetConnectionString("MvcDemoConnectionString")));
 
 var app = builder.Build();
 
